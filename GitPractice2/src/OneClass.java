57352dd5298c6fb6eb3d36fc43d8bca62731d5bb
0601ahmad <ghafooriahmadfarhad@gmail.com>
2022-01-05 21:11:47 -0800

    this is a new project

---
+++ GitPractice2/src/OneClass.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class OneClass {
+
+	public static void main(String[] args) {
+		
+		int [] num = {1,1,2,2,3,3,4,4,5,6,7,8,8,9,9};
+		Set<Integer> set = new HashSet<>();
+		for (int i=0;i<num.length;i++) {
+			set.add(num[i]);
+		}
+		System.out.println(set);
+	}
+
+}
