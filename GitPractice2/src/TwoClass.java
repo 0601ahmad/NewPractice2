80a6d59af8226cfe75e20eb7d5ed0c4652515e5d
0601ahmad <ghafooriahmadfarhad@gmail.com>
2022-01-05 21:18:51 -0800

    new class

---
+++ GitPractice2/src/TwoClass.java
@@ -0,0 +1,14 @@
+
+public class TwoClass {
+
+	public static void main(String[] args) {
+		int[] num = {1,2,3,4,5,6,7,8};
+		
+		for (int i=num.length-1;i>=0;i--) {
+			
+			System.out.println(num[i]);
+		}
+
+	}
+
+}
